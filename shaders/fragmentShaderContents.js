bce3107a51a791bff0f60e1974a447d0d7840651
GrubySamuraj <jakub.dragosz@o2.pl>
2025-06-10 16:48:41 +0200

    light added to shaders

--- shaders/fragmentShaderContents.js
+++ shaders/fragmentShaderContents.js
@@ -1,12 +1,21 @@
-const fragmentShaderContents =
-`
+const fragmentShaderContents = `
 precision mediump float;
 
 varying vec2 fragTextureCoord;
+varying vec3 fragmentNormal;
+
+uniform vec3 ambientLightIntensity;
+uniform vec3 lightDirection;
+uniform vec3 lightColor;
+
 
 uniform sampler2D sampler;
 
 void main() {
-    gl_FragColor = texture2D(sampler, fragTextureCoord);
+    vec4 texel = texture2D(sampler, fragTextureCoord);
+
+    vec3 lightIntensity = ambientLightIntensity + lightColor * max(dot(fragmentNormal, lightDirection), 0.0);
+
+    gl_FragColor = vec4(texel.rgb * lightIntensity, texel.a);
 }
-`
+`;
--- shaders/vertexShaderContents.js
+++ shaders/vertexShaderContents.js
@@ -1,19 +1,22 @@
-const vertexShaderContents =
-`
+const vertexShaderContents = `
 precision mediump float;
 
 attribute vec3 vertPosition;
 attribute vec2 textureCoord;
+attribute vec3 vertNormal;
 
 uniform mat4 mModel;
 uniform mat4 mView;
 uniform mat4 mProj;
 
 varying vec2 fragTextureCoord;
+varying vec3 fragmentNormal;
 
 void main() {
     fragTextureCoord = textureCoord;
 
+    fragmentNormal = vertNormal;
+
     gl_Position = mProj * mView * mModel * vec4(vertPosition, 1.0);
 }
-`
+`;
